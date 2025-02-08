b9d1863e6e008c8a074df310323cbfdfe046e63b
Nhat Quang HO <72070122+nhatquang-ho@users.noreply.github.com>
2025-02-08 22:47:53 +0100

    init project

---
+++ src/main/java/backend/SaleCalc_back/SaleCalcBackApplication.java
@@ -0,0 +1,13 @@
+package backend.SaleCalc_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleCalcBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaleCalcBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/SaleCalc_back/SaleCalcBackApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.SaleCalc_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleCalcBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
